0e89449d07f9194f41625cbd56812a805b693bdb
Ambady <cssreedeep@gmail.com>
2023-05-28 22:31:24 +0530

    Edit menu

--- backend/services/AdmEditMenuSer.js
+++ backend/services/AdmEditMenuSer.js
@@ -2,7 +2,7 @@ import db from '../models/Database.js'
 
 export const admEditMenuSer =()=>{
     return new Promise((resolve,reject)=>{
-        const q = 'SELECT * FROM menu'
+        const q = "SELECT day, breakfast, lunch, dinner FROM menu ORDER BY FIELD(day, 'Sunday' , 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')"
         db.query(q,(err,result)=>{
             if(err){
                 reject(err)
